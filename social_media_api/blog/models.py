f54c87556f2cd9e5948b21d2eba7939dd48b9d15
Yaser Dehghan <ydahgan@gmail.com>
2024-09-05 09:00:56 +0330

    change the abstract model of Product to BaseModel

--- social_media_api/blog/models.py
+++ social_media_api/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from social_media_api.common.models import BaseModel
 
 
-class Product(models.Model):
+class Product(BaseModel):
     name = models.CharField(max_length=10)
