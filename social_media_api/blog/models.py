a66e124ac3c90f27453257054894fc02d2ba6875
Yaser Dehghan <ydahgan@gmail.com>
2024-09-26 08:02:02 +0330

    Create Post and Subscribe models

--- social_media_api/blog/models.py
+++ social_media_api/blog/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from social_media_api.common.models import BaseModel
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Product(BaseModel):
     name = models.CharField(max_length=10)
+
+
+class Post(BaseModel):
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class Subscribe(BaseModel):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+
