e8f607df218e637ef78bc96522a92c170c52cdb7
Yaser Dehghan <ydahgan@gmail.com>
2024-09-27 13:03:11 +0330

    add post urls

--- social_media_api/blog/urls.py
+++ social_media_api/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .api.product import ProductView
+from .api.post import PostApi, PostDetailApi
 
+app_name = 'blog'
 
 urlpatterns = [
-    path("product", ProductView.as_view(),  name="product")
+    path("product", ProductView.as_view(),  name="product"),
+    path("post/", PostApi.as_view(), name="post"),
+    path("post/<slug:slug>", PostDetailApi.as_view(), name="post_detail" )
 ]
