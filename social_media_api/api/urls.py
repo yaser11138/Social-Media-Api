08eeb601e620bd12d0c0420cd8a734b8274bc81f
Yaser Dehghan <ydahgan@gmail.com>
2024-09-27 13:00:29 +0330

    add authentication urls to api

--- social_media_api/api/urls.py
+++ social_media_api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include(('social_media_api.blog.urls', 'blog')))
+    path('blog/', include('social_media_api.blog.urls')),
+    path("auth/", include("social_media_api.authentication.urls")),
+    path("users/", include("social_media_api.users.urls", ))
 
 ]
