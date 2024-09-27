25438c88c7c1c1ed4c445e3c33d3417865c68893
Yaser Dehghan <ydahgan@gmail.com>
2024-09-27 13:01:15 +0330

    add name app to users urls

--- social_media_api/users/urls.py
+++ social_media_api/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .apis import ProfileApi, RegisterApi
+from rest_framework_simplejwt.views import TokenObtainPairView
 
+app_name = 'users'
 
 urlpatterns = [
     path('register/', RegisterApi.as_view(),name="register"),
