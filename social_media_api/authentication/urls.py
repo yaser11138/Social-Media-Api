73df6f31b5318d9fcdf0038291bd06da115f4848
Yaser Dehghan <ydahgan@gmail.com>
2024-09-27 13:00:58 +0330

    add name app to authentication urls

--- social_media_api/authentication/urls.py
+++ social_media_api/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+app_name = 'authentication'
+
 urlpatterns = [
         path('jwt/', include(([
             path('login/', TokenObtainPairView.as_view(),name="login"),
