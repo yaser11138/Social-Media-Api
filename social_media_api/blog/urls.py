aecf10d1c443b09ab19d16193dd280f3b3a60b07
Yaser Dehghan <ydahgan@gmail.com>
2024-09-30 10:33:44 +0330

    ADD Subscribe urls

--- social_media_api/blog/urls.py
+++ social_media_api/blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .api.product import ProductView
 from .api.post import PostApi, PostDetailApi
+from .api.subscribe import SubscribeApi, SubscribeDetailApi
 
 app_name = 'blog'
 
 urlpatterns = [
     path("product", ProductView.as_view(),  name="product"),
     path("post/", PostApi.as_view(), name="post"),
-    path("post/<slug:slug>", PostDetailApi.as_view(), name="post_detail" )
+    path("post/<slug:slug>", PostDetailApi.as_view(), name="post-detail"),
+    path("subscribe/", SubscribeApi.as_view(), name="subscribe"),
+    path("subscribe/<uuid:uuid>",SubscribeDetailApi.as_view(), name="subscribe-detail")
 ]
